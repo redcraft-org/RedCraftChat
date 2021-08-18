d61f94f4f81c5c8133eb9bb18b21b35245bbc678
lululombard <lululombard@gmail.com>
2021-08-18 02:42:04 +0000

    Start to handle Minecraft messages ourselves

--- src/main/java/org/redcraft/redcraftchat/listeners/minecraft/MinecraftChatListener.java
+++ src/main/java/org/redcraft/redcraftchat/listeners/minecraft/MinecraftChatListener.java
@@ -1,5 +1,10 @@
 package org.redcraft.redcraftchat.listeners.minecraft;
 
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.ChatEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
@@ -9,6 +14,18 @@ public class MinecraftChatListener implements Listener {
 
     @EventHandler(priority = EventPriority.NORMAL)
     public void onChatEvent(ChatEvent event) {
-        // TODO check commands and tchat
+        // TODO check commands and chat
+        if (event.isProxyCommand() || event.isCommand() || !(event.getSender() instanceof ProxiedPlayer)) {
+            return;
+        }
+
+        ProxiedPlayer player = (ProxiedPlayer) event.getSender();
+
+        // TODO do actual formatting
+        BaseComponent[] message = new ComponentBuilder("[" + player.getDisplayName() + ChatColor.RESET + "] " + event.getMessage()).create();
+
+        ProxyServer.getInstance().broadcast(message);
+
+        event.setCancelled(true);
     }
 }
