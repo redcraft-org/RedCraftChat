01f80e207221d8e732e42b45b8056080938de0ee
lululombard <lululombard@gmail.com>
2023-01-27 16:01:27 +0000

    Use official Deepl client

--- src/main/java/org/redcraft/redcraftchat/translate/providers/DeeplProvider.java
+++ src/main/java/org/redcraft/redcraftchat/translate/providers/DeeplProvider.java
@@ -2,48 +2,33 @@ package org.redcraft.redcraftchat.translate.providers;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.text.Normalizer.Form;
 
-import com.google.gson.Gson;
+import com.deepl.api.DeepLException;
+import com.deepl.api.Formality;
+import com.deepl.api.TextResult;
+import com.deepl.api.TextTranslationOptions;
+import com.deepl.api.Translator;
 
-import org.apache.http.client.utils.URIBuilder;
 import org.redcraft.redcraftchat.Config;
 import org.redcraft.redcraftchat.RedCraftChat;
 import org.redcraft.redcraftchat.caching.CacheManager;
 import org.redcraft.redcraftchat.models.caching.CacheCategory;
-import org.redcraft.redcraftchat.models.deepl.DeeplResponse;
-import org.redcraft.redcraftchat.models.deepl.DeeplTranslation;
-import org.redcraft.redcraftchat.models.deepl.DeeplSupportedLanguage;
 
 public class DeeplProvider implements TranslationProvider {
 
-    private HttpClient httpClient;
-    private HashMap<String, DeeplSupportedLanguage> supportedLanguages;
+    private Translator translator;
 
     public DeeplProvider() {
-        httpClient = HttpClient.newHttpClient();
-
-        supportedLanguages = new HashMap<>();
-        supportedLanguages.put("EN", new DeeplSupportedLanguage("EN", false));
-        supportedLanguages.put("FR", new DeeplSupportedLanguage("FR", true));
-        supportedLanguages.put("DE", new DeeplSupportedLanguage("DE", true));
-        supportedLanguages.put("IT", new DeeplSupportedLanguage("IT", true));
-        supportedLanguages.put("JA", new DeeplSupportedLanguage("JA", false));
-        supportedLanguages.put("ES", new DeeplSupportedLanguage("ES", false));
-        supportedLanguages.put("NL", new DeeplSupportedLanguage("NL", true));
-        supportedLanguages.put("PL", new DeeplSupportedLanguage("PL", true));
-        supportedLanguages.put("PT", new DeeplSupportedLanguage("PT", true));
-        supportedLanguages.put("RU", new DeeplSupportedLanguage("RU", true));
-        supportedLanguages.put("ZH", new DeeplSupportedLanguage("ZH", false));
+        this.translator = new Translator(Config.deeplToken);
     }
 
     public String translate(String text, String sourceLanguageId, String targetLanguageId) throws IllegalStateException, URISyntaxException, IOException, InterruptedException {
         String sourceLangId = sourceLanguageId.toLowerCase().split("-")[0];
-        String targetLangId = targetLanguageId.toLowerCase().split("-")[0];
+        String targetLangId = targetLanguageId.toLowerCase();
+        if (targetLangId.equals("en")) {
+            targetLangId = "en-US";
+        }
         String cacheKey = String.format("%s;%s;%s", sourceLangId, targetLangId, text);
 
         String cachedDeeplResponse = (String) CacheManager.get(CacheCategory.DEEPL_TRANSLATED_MESSAGE, cacheKey, String.class);
@@ -52,60 +37,39 @@ public class DeeplProvider implements TranslationProvider {
             return cachedDeeplResponse;
         }
 
-        DeeplSupportedLanguage sourceLang = getLanguage(sourceLangId);
-        DeeplSupportedLanguage targetLang = getLanguage(targetLangId);
+        TextTranslationOptions translationOptions = new TextTranslationOptions();
 
-        if (sourceLang == null) {
-            throw new IllegalStateException("The source language " + sourceLangId + " is not supported by Deepl");
+        switch (Config.deeplFormality) {
+            case "less":
+                translationOptions.setFormality(Formality.PreferLess);
+                break;
+
+            case "more":
+                translationOptions.setFormality(Formality.PreferMore);
+                break;
+
+            default:
+                break;
         }
 
-        if (targetLang == null) {
-            throw new IllegalStateException("The source language " + sourceLangId + " is not supported by Deepl");
+        translationOptions.setPreserveFormatting(Config.deeplPreserveFormatting);
+
+        TextResult result;
+        try {
+            result = translator.translateText(text, sourceLangId, targetLangId, translationOptions);
+        } catch (DeepLException e) {
+            e.printStackTrace();
+            throw new IllegalStateException("Could not translate text from " + sourceLangId + " to "  + targetLangId + " with Deepl: " + e.getMessage());
         }
-
-        URIBuilder url = new URIBuilder(Config.deeplEndpoint);
-        url.addParameter("auth_key", Config.deeplToken);
-        url.addParameter("text", text);
-        url.addParameter("source_lang", sourceLang.languageId);
-        url.addParameter("target_lang", targetLang.languageId);
-        url.addParameter("preserve_formatting", Config.deeplPreserveFormatting ? "1" : "0");
-
-        if (targetLang.formalityAvailable) {
-            url.addParameter("formality", Config.deeplFormality);
-        }
-
-        HttpRequest request = HttpRequest.newBuilder(url.build())
-                .header("accept", "application/json")
-                .header("content-type", "application/json")
-                .header("Autorization", "DeepL-Auth-Key " + Config.deeplToken)
-                .GET()
-                .build();
-
-        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-
-        DeeplResponse deeplResponse = new Gson().fromJson(response.body(), DeeplResponse.class);
 
         // TODO remove debug
         String debugMessage = "Used " + text.length() + " Deepl chars to translate to " + targetLangId;
         RedCraftChat.getInstance().getLogger().info(debugMessage);
 
-        ArrayList<String> translations = new ArrayList<String>();
-        for (DeeplTranslation translation : deeplResponse.translations) {
-            translations.add(translation.text);
-        }
-
-        String translated = String.join(" ", translations);
+        String translated = result.getText();
 
         CacheManager.put(CacheCategory.DEEPL_TRANSLATED_MESSAGE, cacheKey, translated);
 
         return translated;
-    }
-
-    private DeeplSupportedLanguage getLanguage(String id) throws IllegalStateException {
-        try {
-            return supportedLanguages.get(id.toUpperCase());
-        } catch (Exception ex) {
-            throw new IllegalStateException("Could not find language %s in the supported languages: " + id, ex.fillInStackTrace());
-        }
     }
 }
