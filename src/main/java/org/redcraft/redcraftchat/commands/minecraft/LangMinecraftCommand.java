e95aaa056e3c2a164dc5f9ebda11ed462824db5b
lululombard <lululombard@gmail.com>
2026-08-22 20:23:06 +0000

    Rebuild the language selector menu in Adventure

--- src/main/java/org/redcraft/redcraftchat/commands/minecraft/LangMinecraftCommand.java
+++ src/main/java/org/redcraft/redcraftchat/commands/minecraft/LangMinecraftCommand.java
@@ -6,6 +6,9 @@ import java.util.List;
 
 import org.redcraft.redcraftchat.RedCraftChat;
 import org.redcraft.redcraftchat.helpers.BasicMessageFormatter;
+import org.redcraft.redcraftchat.helpers.LegacyText;
+import org.redcraft.redcraftchat.locales.LocaleManager;
+import org.redcraft.redcraftchat.models.locales.SupportedLocale;
 import org.redcraft.redcraftchat.models.players.PlayerPreferences;
 import org.redcraft.redcraftchat.players.PlayerPreferencesManager;
 
@@ -14,6 +17,9 @@ import com.velocitypowered.api.command.SimpleCommand;
 import com.velocitypowered.api.proxy.Player;
 
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.event.ClickEvent;
+import net.kyori.adventure.text.event.HoverEvent;
+import net.kyori.adventure.text.format.NamedTextColor;
 
 public class LangMinecraftCommand implements SimpleCommand {
 
@@ -59,10 +65,91 @@ public class LangMinecraftCommand implements SimpleCommand {
     }
 
     private List<Component> generateMenu(PlayerPreferences preferences) {
-        // TODO wave 2: port the interactive language selector menu
-        // (header, caption, per locale toggle with hover tooltips and run-command
-        // click events) from the BungeeCord ComponentBuilder API to Adventure
-        return new ArrayList<Component>();
+        String originalHeaderText = "LANGUAGE SELECTOR";
+        String originalHelpText = "Click on a language to enable or disable it, click on the checkbox to make it default.";
+        String originalCaptionText = "Caption:";
+        String originalDisabledText = "disabled";
+        String originalEnabledText = "enabled";
+
+        String headerText = PlayerPreferencesManager.localizeMessageForPlayer(preferences, originalHeaderText);
+        String helpText = PlayerPreferencesManager.localizeMessageForPlayer(preferences, originalHelpText);
+        String captionText = PlayerPreferencesManager.localizeMessageForPlayer(preferences, originalCaptionText);
+        String disabledText = PlayerPreferencesManager.localizeMessageForPlayer(preferences, originalDisabledText);
+        String enabledText = PlayerPreferencesManager.localizeMessageForPlayer(preferences, originalEnabledText);
+
+        String alreadyMainLanguage = PlayerPreferencesManager.localizeMessageForPlayer(preferences, "This is already your main language");
+        String setAsMainLanguage = PlayerPreferencesManager.localizeMessageForPlayer(preferences, "Click to set as main language");
+        String removeFromLanguages = PlayerPreferencesManager.localizeMessageForPlayer(preferences, "Click to remove this languages");
+        String addToLanguages = PlayerPreferencesManager.localizeMessageForPlayer(preferences, "Click to add this language");
+
+        List<Component> messages = new ArrayList<Component>();
+
+        // Add 5 empty lines to make the menu look better
+        for (int i = 0; i < 5; i++) {
+            messages.add(Component.empty());
+        }
+
+        // BungeeCord copied the hover of the previous part into every part that did
+        // not set one of its own, Adventure has no such cascade between siblings so
+        // the inherited events are repeated explicitly below
+        HoverEvent<Component> headerHover = showText(originalHeaderText);
+        messages.add(Component.text()
+                .append(Component.text("---------- ", NamedTextColor.GREEN))
+                .append(Component.text(headerText, NamedTextColor.GOLD).hoverEvent(headerHover))
+                .append(Component.text(" ----------", NamedTextColor.GREEN).hoverEvent(headerHover))
+                .build());
+
+        messages.add(Component.text(helpText, NamedTextColor.YELLOW).hoverEvent(showText(originalHelpText)));
+
+        HoverEvent<Component> captionHover = showText(originalCaptionText);
+        HoverEvent<Component> disabledHover = showText(originalDisabledText);
+        messages.add(Component.text()
+                .append(Component.text(captionText, NamedTextColor.GOLD).hoverEvent(captionHover))
+                .append(Component.text(" ", NamedTextColor.GOLD).hoverEvent(captionHover))
+                .append(Component.text(disabledText, NamedTextColor.GRAY).hoverEvent(disabledHover))
+                .append(Component.text(" ", NamedTextColor.GRAY).hoverEvent(disabledHover))
+                .append(Component.text(enabledText, NamedTextColor.GREEN).hoverEvent(showText(originalEnabledText)))
+                .build());
+
+        for (SupportedLocale locale : LocaleManager.getSupportedLocales()) {
+            boolean isMainLanguage = locale.code.equals(preferences.mainLanguage);
+
+            Component checkbox;
+            if (isMainLanguage) {
+                checkbox = Component.text("[X] ", NamedTextColor.GREEN)
+                        .hoverEvent(showText(LegacyText.RED + alreadyMainLanguage));
+            } else {
+                checkbox = Component.text("[ ] ", NamedTextColor.DARK_GRAY)
+                        .clickEvent(ClickEvent.runCommand("/lang " + locale.code + " main"))
+                        .hoverEvent(showText(LegacyText.GREEN + setAsMainLanguage));
+            }
+
+            Component localeName = Component.text(locale.name);
+            if (preferences.languages.contains(locale.code)) {
+                localeName = localeName.color(NamedTextColor.GREEN);
+                if (isMainLanguage) {
+                    // Inherited from the checkbox, the main language row explains
+                    // on hover why nothing on it can be clicked
+                    localeName = localeName.hoverEvent(showText(LegacyText.RED + alreadyMainLanguage));
+                } else {
+                    localeName = localeName.hoverEvent(showText(LegacyText.RED + removeFromLanguages));
+                }
+            } else {
+                localeName = localeName.color(NamedTextColor.GRAY)
+                        .hoverEvent(showText(LegacyText.GREEN + addToLanguages));
+            }
+            if (!isMainLanguage) {
+                localeName = localeName.clickEvent(ClickEvent.runCommand("/lang " + locale.code));
+            }
+
+            messages.add(Component.text().append(checkbox).append(localeName).build());
+        }
+
+        return messages;
+    }
+
+    private HoverEvent<Component> showText(String legacyText) {
+        return HoverEvent.showText(BasicMessageFormatter.deserialize(legacyText));
     }
 
     private void toggleLocale(PlayerPreferences preferences, Player player, String locale) {
